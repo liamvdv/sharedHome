fee61a43ff583c7923cb83790279e60932231d12
liamvdv <liam.vanderviven@gmx.de>
2021-08-01 00:06:45 +0200

    update util/helpers

--- util/helpers.go
+++ util/helpers.go
@@ -2,42 +2,43 @@ package util
 
 import (
 	"errors"
-	"io/fs"
 	"os"
-	"path/filepath"
+
+	"github.com/liamvdv/sharedHome/osx"
 )
 
 // TODO(liamvdv): super difficult to get 100% right.
-func Exists(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil || !errors.Is(err, fs.ErrNotExist)
+
+func Exists(fs osx.Fs, path string) bool {
+	_, err := fs.Stat(path)
+	return err == nil || !errors.Is(err, os.ErrNotExist)
 }
 
-func SaveClose(f *os.File) {
+func SaveClose(f osx.File) {
 	if err := f.Close(); err != nil {
 		panic(err)
 	}
 }
 
 // PrepareTestConfigInit makes the working directory the --/sharedHome project root.
-func PrepareTestConfigInit() {
-	fp, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	for {
-		name := filepath.Base(fp)
-		if name == "sharedHome" {  // src root dir
-			break
-		}
-		if len(fp) < 3 { // no valid known path.
-			panic("cannot find sharedHome root directory.")
-		}
+// func PrepareTestConfigInit() {
+// 	fp, err := os.Getwd()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	for {
+// 		name := filepath.Base(fp)
+// 		if name == "sharedHome" { // src root dir
+// 			break
+// 		}
+// 		if len(fp) < 3 { // no valid known path.
+// 			panic("cannot find sharedHome root directory.")
+// 		}
 
-		fp = fp[:len(fp)-1-len(name)] // -1 for '/' or '\'
-	}
+// 		fp = fp[:len(fp)-1-len(name)] // -1 for '/' or '\'
+// 	}
 
-	if err := os.Chdir(fp); err != nil {
-		panic(err)
-	}
-}
+// 	if err := os.Chdir(fp); err != nil {
+// 		panic(err)
+// 	}
+// }
